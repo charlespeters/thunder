3d40d9225c416aa78e2957ace2e8097457ca4829
Sergey Lukin <contact@sergeylukin.com>
2015-08-24 18:20:22 +0300

    Wait for Modernizr to perform WebP test and only then run Thunder if test failes

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 var Modernizr = require('./src/modernizr-custom');
 var Thunder = require('./src/thunder-images');
 
-if (!Modernizr.webp) {
-  new Thunder();
-}
+Modernizr.on('webp', function (webp) {
+  if (!webp) {
+    new Thunder();
+  }
+});
